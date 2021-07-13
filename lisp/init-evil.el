166ee87fe7b8c8d7580ae300e9daba88903ef82b
cc <cc20070411@163.com>
2021-07-13 13:37:17 +0800

    update evil config

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -29,13 +29,45 @@
 (setq undo-limit 8000000
       undo-strong-limit 8000000
       undo-outer-limit 8000000)
-;; nerd commenter
-(use-package evil-nerd-commenter)
-(evilnc-default-hotkeys) ;; default keybinding
-;; surround
+
+;; {{ evil-surround @see https://github.com/timcharper/evil-surround for tutorial
 (use-package evil-surround
   :ensure t
   :config
   (global-evil-surround-mode 1))
+(with-eval-after-load 'evil-surround
+  (defun evil-surround-prog-mode-hook-setup ()
+    "Set up surround shortcuts."
+    (cond
+     ((memq major-mode '(sh-mode))
+      (push '(?$ . ("$(" . ")")) evil-surround-pairs-alist))
+     (t
+      (push '(?$ . ("${" . "}")) evil-surround-pairs-alist)))
+
+    (when (memq major-mode '(org-mode))
+      (push '(?\[ . ("[[" . "]]")) evil-surround-pairs-alist) ; [
+      (push '(?= . ("=" . "=")) evil-surround-pairs-alist))
+
+    (when (memq major-mode '(emacs-lisp-mode))
+      (push '(?\( . ("( " . ")")) evil-surround-pairs-alist)
+      (push '(?` . ("`" . "'")) evil-surround-pairs-alist))
+
+    (when (or (derived-mode-p 'js-mode)
+              (memq major-mode '(typescript-mode web-mode)))
+      (push '(?j . ("JSON.stringify(" . ")")) evil-surround-pairs-alist)
+      (push '(?> . ("(e) => " . "(e)")) evil-surround-pairs-alist))
+
+    ;; generic
+    (push '(?/ . ("/" . "/")) evil-surround-pairs-alist))
+  (add-hook 'prog-mode-hook 'evil-surround-prog-mode-hook-setup))
+;; }}
+
+;; {{ evil-nerd-commenter
+(use-package evil-nerd-commenter)
+(evilnc-default-hotkeys) ;; default keybinding
+(define-key evil-motion-state-map "gc" 'evilnc-comment-operator) ; same as doom-emacs
+(define-key evil-motion-state-map "gb" 'evilnc-copy-and-comment-operator)
+(define-key evil-motion-state-map "gy" 'evilnc-yank-and-comment-operator)
+;; }}
 
 (provide 'init-evil)
